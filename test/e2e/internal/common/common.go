142148eeac96cb19b1dd462002cba4147e318e7b
Julian Vanden Broeck <julian.vandenbroeck@dalibo.com>
2025-11-07 16:13:54 +0100

    Add test for backup action

---
+++ test/e2e/internal/common/common.go
@@ -0,0 +1,28 @@
+package common
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("vim-go")
+}
+
+type Retrier struct {
+	MaxRetry      uint
+	Interval      uint
+	Sleep         time.Duration
+	ErrorCounters uint
+}
+
+func NewRetrier(maxRetry uint) (*Retrier, error) {
+	if maxRetry == 0 {
+		return nil, fmt.Errorf("maxRetry should be non-zero value")
+	}
+	return &Retrier{MaxRetry: maxRetry, Interval: 1, Sleep: time.Second}, nil
+}
+
+func (r Retrier) Wait() {
+	time.Sleep(time.Duration(r.Interval) * r.Sleep)
+}
