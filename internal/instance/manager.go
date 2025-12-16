8e369fa327733cc33e539d24b8e4c594f1463234
Julian Vanden Broeck <julian.vandenbroeck@dalibo.com>
2025-12-16 15:40:46 +0100

    Copy how scheme are added from barman plugin

--- internal/instance/manager.go
+++ internal/instance/manager.go
@@ -14,20 +14,14 @@ import (
 	"github.com/spf13/viper"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/scheme"
 )
-
-var scheme = runtime.NewScheme()
-
-func init() {
-	utilruntime.Must(apipgbackrest.AddToScheme(scheme))
-	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
-	utilruntime.Must(cnpgv1.AddToScheme(scheme))
-}
 
 // Start starts the sidecar informers and pgbackrest plugin
 // server. This is the part running on the sidecar container.
@@ -36,8 +30,9 @@ func Start(ctx context.Context) error {
 	setupLog.Info("Starting pgbackrest instance plugin")
 	podName := viper.GetString("pod-name")
 
+	sc := generateScheme(ctx)
 	controllerOptions := ctrl.Options{
-		Scheme: scheme,
+		Scheme: sc,
 		Client: client.Options{
 			Cache: &client.CacheOptions{
 				DisableFor: []client.Object{
@@ -72,4 +67,35 @@ func Start(ctx context.Context) error {
 	}
 
 	return nil
+}
+
+// generateScheme creates a runtime.Scheme object with all the
+// definition needed to support the sidecar. This allows
+// the plugin to be used in every CNPG-based operator.
+func generateScheme(ctx context.Context) *runtime.Scheme {
+	result := runtime.NewScheme()
+	utilruntime.Must(apipgbackrest.AddToScheme(result))
+	utilruntime.Must(clientgoscheme.AddToScheme(result))
+
+	cnpgGroup := viper.GetString("custom-cnpg-group")
+	cnpgVersion := viper.GetString("custom-cnpg-version")
+	if len(cnpgGroup) == 0 {
+		cnpgGroup = cnpgv1.SchemeGroupVersion.Group
+	}
+	if len(cnpgVersion) == 0 {
+		cnpgVersion = cnpgv1.SchemeGroupVersion.Version
+	}
+
+	// Proceed with custom registration of the CNPG scheme
+	schemeGroupVersion := schema.GroupVersion{Group: cnpgGroup, Version: cnpgVersion}
+	schemeBuilder := &scheme.Builder{GroupVersion: schemeGroupVersion}
+	schemeBuilder.Register(&cnpgv1.Cluster{}, &cnpgv1.ClusterList{})
+	schemeBuilder.Register(&cnpgv1.Backup{}, &cnpgv1.BackupList{})
+	schemeBuilder.Register(&cnpgv1.ScheduledBackup{}, &cnpgv1.ScheduledBackupList{})
+	utilruntime.Must(schemeBuilder.AddToScheme(result))
+
+	schemeLog := log.FromContext(ctx)
+	schemeLog.Info("CNPG types registration", "schemeGroupVersion", schemeGroupVersion)
+
+	return result
 }
