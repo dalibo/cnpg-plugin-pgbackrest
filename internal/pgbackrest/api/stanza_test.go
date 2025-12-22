b301a50f338bade4ac62555ad52dc97e584db436
Julian Vanden Broeck <julian.vandenbroeck@dalibo.com>
2025-12-22 10:35:03 +0100

    Add test for stanza/pgbackrest conf to env var

---
+++ internal/pgbackrest/api/stanza_test.go
@@ -0,0 +1,71 @@
+// SPDX-FileCopyrightText: 2025 Dalibo <contact@dalibo.com>
+//
+// SPDX-License-Identifier: Apache-2.0
+package api
+
+import (
+	"slices"
+	"testing"
+
+	"k8s.io/utils/ptr"
+)
+
+func TestStanzaToEnv(t *testing.T) {
+	expected := []string{
+		"PGBACKREST_REPO1_S3_BUCKET=demo",
+		"PGBACKREST_REPO1_S3_ENDPOINT=s3.minio.svc.cluster.local",
+		"PGBACKREST_REPO1_S3_REGION=us-east-1",
+		"PGBACKREST_REPO1_S3_URI_STYLE=path",
+		"PGBACKREST_REPO1_S3_VERIFY_TLS=n",
+		"PGBACKREST_REPO1_PATH=/cluster-demo",
+		"PGBACKREST_REPO2_S3_BUCKET=demo2",
+		"PGBACKREST_REPO2_S3_ENDPOINT=s3.minio.svc.cluster.local",
+		"PGBACKREST_REPO2_S3_REGION=us-east-1",
+		"PGBACKREST_REPO2_S3_VERIFY_TLS=n",
+		"PGBACKREST_REPO2_PATH=/cluster-demo2",
+		"PGBACKREST_REPO2_CIPHER_TYPE=aes-256-cbc",
+		"PGBACKREST_STANZA=main",
+		"PGBACKREST_ARCHIVE_ASYNC=y",
+		"PGBACKREST_COMPRESS_TYPE=gz",
+		"PGBACKREST_COMPRESS_LEVEL=8",
+		"PGBACKREST_lock-path=/controller/tmp/pgbackrest-cnpg-plugin.lock",
+	}
+	b := false
+	r := Stanza{
+		Name: "main",
+		S3Repositories: []S3Repository{
+			{
+				Bucket:    "demo",
+				Endpoint:  "s3.minio.svc.cluster.local",
+				Region:    "us-east-1",
+				RepoPath:  "/cluster-demo",
+				UriStyle:  "path",
+				VerifyTLS: &b,
+			},
+			{
+				Bucket:    "demo2",
+				Endpoint:  "s3.minio.svc.cluster.local",
+				Region:    "us-east-1",
+				RepoPath:  "/cluster-demo2",
+				VerifyTLS: &b,
+				Cipher: &CipherConfig{
+					Type: "aes-256-cbc",
+				},
+			},
+		},
+		Archive: ArchiveOption{
+			Async: true,
+		},
+		Compress: &CompressConfig{
+			Type:  ptr.To("gz"),
+			Level: 8,
+		},
+	}
+	res, err := r.ToEnv()
+	if err != nil {
+		t.Errorf("got error when converting to env var, %s", err.Error())
+	}
+	if !slices.Equal(res, expected) {
+		t.Errorf("Expected %v, but got %v", expected, res)
+	}
+}
